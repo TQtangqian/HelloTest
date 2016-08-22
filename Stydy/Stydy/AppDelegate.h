357f67f0376966b077d822ceeb83d9af8370b24c
民创联盟 <1371252715@qq.com>
2016-08-22 17:16:18 +0800

    新建一个项目

---
+++ Stydy/Stydy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stydy
+//
+//  Created by 民创联盟 on 16/8/22.
+//  Copyright © 2016年 TangQian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stydy/Stydy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stydy
+//
+//  Created by 民创联盟 on 16/8/22.
+//  Copyright © 2016年 TangQian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
